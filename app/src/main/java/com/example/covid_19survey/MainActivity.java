902608ae1e67ac2c424e850f305ea3f2f42f0d19
Sarp Ulas Kaya <sarpkaya2000@gmail.com>
2022-03-30 20:37:09 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/covid_19survey/MainActivity.java
+++ app/src/main/java/com/example/covid_19survey/MainActivity.java
@@ -5,13 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.RadioButton;
 import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import android.widget.DatePicker;
 import android.widget.TextView;
 
@@ -21,14 +23,15 @@ public class MainActivity extends AppCompatActivity {
     EditText city;
     EditText sideEffect,sym;
     Button register;
-    RadioButton male,female,x,bio,sin,none;
+    RadioButton male, female, x, bio, sin, none;
     Button submit;
     private Button selectDate;
     private DatePickerDialog datePickerDialog;
     private TextView dateTxt;
     private Calendar calendar;
     private int year, month, dayOfMonth;
-    boolean flag = false;
+    boolean datePicked = false;
+    boolean dateValid = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +50,20 @@ public class MainActivity extends AppCompatActivity {
         submit = (Button)findViewById(R.id.buttonRegister);
         selectDate = findViewById(R.id.selectDate);
         dateTxt = findViewById(R.id.dateTxt);
+        View.OnClickListener showSideEffects = new View.OnClickListener() {
+            public void onClick(View v) {
+                sideEffect.setVisibility(View.VISIBLE);
+            }
+
+        };
+        View.OnClickListener hideSideEffects = new View.OnClickListener() {
+            public void onClick(View v) {
+                sideEffect.setVisibility(View.GONE);
+            }
+        };
+        bio.setOnClickListener(showSideEffects);
+        sin.setOnClickListener(showSideEffects);
+        none.setOnClickListener(hideSideEffects);
         selectDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -59,7 +76,10 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
                                 dateTxt.setText("Birthday:"+ day + "/" + (month+1) + "/" + year);
-                                flag = true;
+                                datePicked = true;
+                                if ( (year < 2022) && (year > 1900) ) {
+                                    dateValid = true;
+                                }
                             }
                         }, year, month, dayOfMonth);
                 datePickerDialog.show();
@@ -67,58 +87,83 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         submit.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    checkDataEntered();
-
-                }
-            });
-        }
-
-        boolean isEmpty(EditText text) {
-            CharSequence str = text.getText().toString();
-            return TextUtils.isEmpty(str);
-        }
-
-        void checkDataEntered() {
-            if (isEmpty(name)) {
-                Toast t = Toast.makeText(this, "You must enter first name to submit form!", Toast.LENGTH_SHORT);
-                t.show();
-            }
-
-            if (isEmpty(surName)) {
-                Toast t2 = Toast.makeText(this, "You must enter last name to submit form!", Toast.LENGTH_SHORT);
-                t2.show();
-            }
-            if (isEmpty(city)) {
-                Toast t2 = Toast.makeText(this, "You must enter city to submit form!", Toast.LENGTH_SHORT);
-                t2.show();
-            }
-
-
-            if (male.isChecked() || female.isChecked() || x.isChecked()) {
-            } else {
-                Toast.makeText(getApplicationContext(), "Please select Gender", Toast.LENGTH_SHORT).show();
+            @Override
+            public void onClick(View view) {
+                checkDataEntered();
 
             }
-            if (bio.isChecked() || sin.isChecked() || none.isChecked()) {
-            } else {
-                Toast.makeText(getApplicationContext(), "Please select Vaccine Type", Toast.LENGTH_SHORT).show();
-            }
-            if(!isEmpty(name) && !isEmpty(surName) && !isEmpty(city)&&(male.isChecked() || female.isChecked() || x.isChecked()) && (bio.isChecked() || sin.isChecked()) && (flag) ){
-                Toast.makeText(getApplicationContext(), "Submit Done", Toast.LENGTH_SHORT).show();
-            }
-            if(!flag){
-                Toast t2 = Toast.makeText(this, "Please select Birthday", Toast.LENGTH_SHORT);
-                t2.show();
-            }
-            else{
-            }
-
-
-
-
-        }
+        });
     }
 
+    public boolean containsIllegals(EditText text) {
+        CharSequence str = text.getText().toString();
+        Pattern pattern = Pattern.compile("[~#@*+0123456789%{}<>\\[\\]|\"\\_^]");
+        Matcher matcher = pattern.matcher(str);
+        return matcher.find();
+    }
 
+    /*
+    public boolean containsIllegals(EditText toExamine) {
+        CharSequence str = toExamine.getText().toString();
+        for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if(c == '~')
+                return true;
+            else if(c == '#')
+                return true;
+
+            // etc...
+        }
+    }
+    */
+
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+    void checkDataEntered() {
+        if (isEmpty(name)) {
+            Toast t = Toast.makeText(this, "You must enter first name to submit form!", Toast.LENGTH_SHORT);
+            t.show();
+        }
+        else if (containsIllegals(name)) {
+            Toast t = Toast.makeText(this, "You must enter a valid first name!", Toast.LENGTH_SHORT);
+            t.show();
+        }
+        else if (isEmpty(surName)) {
+            Toast t2 = Toast.makeText(this, "You must enter last name to submit form!", Toast.LENGTH_SHORT);
+            t2.show();
+        }
+        else if (containsIllegals(surName)) {
+            Toast t = Toast.makeText(this, "You must enter a valid last name!", Toast.LENGTH_SHORT);
+            t.show();
+        }
+        else if(!datePicked){
+            Toast t2 = Toast.makeText(this, "Please select Birthday", Toast.LENGTH_SHORT);
+            t2.show();
+        }
+        else if(!dateValid){
+            Toast t2 = Toast.makeText(this, "Invalid date of birth, please enter another date", Toast.LENGTH_SHORT);
+            t2.show();
+        }
+        else if (isEmpty(city)) {
+            Toast t2 = Toast.makeText(this, "You must enter city to submit form!", Toast.LENGTH_SHORT);
+            t2.show();
+        }
+        else if (containsIllegals(city)) {
+            Toast t = Toast.makeText(this, "You must enter a valid city name!", Toast.LENGTH_SHORT);
+            t.show();
+        }
+        else if (!male.isChecked() && !female.isChecked() && !x.isChecked()) {
+            Toast.makeText(getApplicationContext(), "Please select Gender", Toast.LENGTH_SHORT).show();
+        }
+        else if (!bio.isChecked() && !sin.isChecked() && !none.isChecked()) {
+            Toast.makeText(getApplicationContext(), "Please select Vaccine Type", Toast.LENGTH_SHORT).show();
+        }
+        else if(!isEmpty(name) && !isEmpty(surName) && !isEmpty(city)&&(male.isChecked() || female.isChecked() || x.isChecked()) && (bio.isChecked() || sin.isChecked() || none.isChecked()) && (datePicked) ){
+            Toast.makeText(getApplicationContext(), "Submit Done", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
